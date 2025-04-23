8ba658e9d83eb6bd87e7d7335649f24252b7dedd
Mark Wilbrink <markwilbrink86@gmail.com>
2025-04-23 10:45:04 +0200

    Added checks, argument 2

--- Backup with robocopy.py
+++ Backup with robocopy.py
@@ -2,30 +2,56 @@ import os
 import sys
 from datetime import datetime
 
-if len(sys.argv) <= 1:
-    sys.exit("No folder was provided by argument!")
-
-source_folder_arg = sys.argv[1]
-
-if os.path.exists(source_folder_arg):
-    drives = os.popen("fsutil fsinfo drives").readlines()
-
-    for drive in list(drives):
-        print(drive)
-
-    drive_chosen = input("On what drive would you like to store the backup?")
-
-    if os.path.exists(drive_chosen):
-        date_today = datetime.now()
-        date_folder = date_today.strftime("%Y-%m-%d")
-
-        source = source_folder_arg
-        destination = f"{drive_chosen}\\Backup\\{date_folder}"
-        options = "/E /R:0 /MIR /A-:SH /XD '.git' '.svn' /XF 'desktop.ini' 'Personal Vault.lnk'"
-
-        print(f"Backup '{source}' to '{destination}'")
-        os.system(f"robocopy {source} {destination} {options}")
+# Function to check if a path exists
+def check_path_exist(path_name):
+    if os.path.exists(path_name):
+        return True
     else:
-        print(f"{drive_chosen} not available!")
+        return False
+
+# Check if argument 1 was provided
+if len(sys.argv) >= 2:
+    source = sys.argv[1]
 else:
-    print(f"{source_folder_arg} not available!")
+    source = ""
+
+# Check if argument 2 was provided
+if len(sys.argv) >= 3:
+    destination = sys.argv[2]
+else:
+    destination = ""
+
+# While loop to check for the source
+while True:
+    if check_path_exist(source) == False:
+        source = input("What would you like to backup? Provide the path: ")
+
+        if check_path_exist(source) == True:
+            break
+    else:
+        break
+
+# While loop to check for the destination
+while True:
+    if check_path_exist(destination) == False:
+        drives = os.popen("fsutil fsinfo drives").readlines()
+
+        for drive in list(drives):
+            print(drive.strip())
+
+        print("")
+        destination = input("Where do you want to store the backup? Provide the path: ")
+        
+        if check_path_exist(destination) == True:
+            break
+    else:
+        break
+
+date_today = datetime.now()
+date_folder = date_today.strftime("%Y-%m-%d")
+
+destination_date = f"{destination}\\{date_folder}"
+options = "/E /R:0 /MIR /A-:SH /XD '.git' '.svn' /XF 'desktop.ini' 'Personal Vault.lnk'"
+
+print(f"Backup '{source}' to '{destination_date}'")
+os.system(f"robocopy {source} {destination_date} {options}")
